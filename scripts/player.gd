479083e34656d07cc5f97fca1f666ead2b5b2ab2
f1uke <26509248+lzy3me@users.noreply.github.com>
2025-06-23 02:39:08 +0700

    added camera and fix tile size

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 @export var speed = 100
-@export var gravity = 200
+@export var gravity = 1800
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	horizontal_movement()
-	
 	move_and_slide()
 
 func horizontal_movement():
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	velocity.x = horizontal_input * speed
+	if $".".position.x < 10:
+		velocity.x = 0
+		$".".position.x = 10
+	else:
+		velocity.x = horizontal_input * speed
